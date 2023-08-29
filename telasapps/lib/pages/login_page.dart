e2df4fca7b4b24d809029f4c7bfc3b7581a74795
limaand <limaand@gmail.com>
2023-08-29 17:03:26 -0300

    corrigindo teclado

--- telasapps/lib/pages/login_page.dart
+++ telasapps/lib/pages/login_page.dart
@@ -13,111 +13,133 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
         child: Scaffold(
       backgroundColor: const Color.fromARGB(255, 224, 240, 217),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          const SizedBox(
-            height: 70,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
           ),
-          Row(
-            children: [
-              Expanded(child: Container()),
-              Expanded(
-                flex: 8,
-                child: Image.network(
-                  //"https://hermes.digitalinnovation.one/assets/diome/logo.png",
-                  "https://www.sescdf.com.br/SiteAssets/marca-sesc-df1/Nova_marca_Sesc_horizontal_CMYK_negativa-01.png",
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            const SizedBox(
+              height: 70,
+            ),
+            Row(
+              children: [
+                Expanded(child: Container()),
+                Expanded(
+                  flex: 8,
+                  child: Image.network(
+                    //"https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                    "https://www.sescdf.com.br/SiteAssets/marca-sesc-df1/Nova_marca_Sesc_horizontal_CMYK_negativa-01.png",
+                  ),
                 ),
-              ),
-              Expanded(child: Container()),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "Já tem cadastro?",
-            style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Faça seu login e make the change_",
-            style: TextStyle(fontSize: 14),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Container(
+                Expanded(child: Container()),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Já tem cadastro?",
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Faça seu login e make the change_",
+              style: TextStyle(fontSize: 14),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
               width: double.infinity,
               margin: const EdgeInsets.symmetric(horizontal: 30),
               height: 30,
               alignment: Alignment.center,
-              child: const Row(
-                children: [
-                  Expanded(flex: 2, child: Text("informe seu e-mail:")),
-                  Expanded(flex: 3, child: Text("email")),
-                ],
-              )),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            height: 30,
-            alignment: Alignment.center,
-            child: const Row(
-              children: [
-                Expanded(flex: 2, child: Text("informe seu senha:")),
-                Expanded(flex: 3, child: Text("senha")),
-                SizedBox(
-                  height: 50,
+              child: const TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 0),
+                    hintText: "E-mail",
+                    hintStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.email_rounded)),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 0),
+                      hintText: "Senha",
+                      hintStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.visibility)),
+                )),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              height: 30,
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                    backgroundColor:
+                        const MaterialStatePropertyAll(Colors.amberAccent),
+                  ),
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400),
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            color: Colors.green,
-            height: 30,
-            alignment: Alignment.center,
-            child: const Text("login"),
-          ),
-          Expanded(child: Container()),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            height: 30,
-            alignment: Alignment.center,
-            child: const Text(
-              "Esqueci minha senha",
-              style: TextStyle(
+            Expanded(child: Container()),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              height: 30,
+              alignment: Alignment.center,
+              child: const Text(
+                "Esqueci minha senha",
+                style: TextStyle(
                   color: Color.fromARGB(255, 209, 155, 18),
-                  fontWeight: FontWeight.w500),
+                ),
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            height: 30,
-            alignment: Alignment.center,
-            child: const Text(
-              "Criar conta",
-              style: TextStyle(
-                  color: Colors.blueAccent, fontWeight: FontWeight.w400),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              height: 30,
+              alignment: Alignment.center,
+              child: const Text(
+                "Criar conta",
+                style: TextStyle(
+                    color: Colors.blueAccent, fontWeight: FontWeight.w400),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ]),
+            const SizedBox(
+              height: 50,
+            ),
+          ]),
+        ),
       ),
     ));
   }
